143fa896b3c4479ac7545ec5d344a9c9bb63f542
Thalita Oliveira <thalita.th.oliveira@gmail.com>
2021-10-18 17:31:23 -0300

    Add login confirmation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  confirmLogin = { 
+    email: '', 
+    password: '',
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  toggle = {
+    errorMessage: false,
+  }
+
+  home() {
+    this.router.navigate(['/home/'])
+  }
+
+  login() {
+    if (this.confirmLogin.email.length > 0 && this.confirmLogin.password.length > 0) {
+      this.router.navigate(['/events/'])
+    } else {
+      this.toggle.errorMessage = true;
+    }
+  }
+
+  closeErrorMessage() {
+    this.toggle.errorMessage = false;
+  }
 }
