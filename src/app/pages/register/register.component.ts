a4bc4f9b8992e747ee94d79e65abb29e5a92c819
Thalita Oliveira <thalita.th.oliveira@gmail.com>
2021-10-18 17:50:28 -0300

    Add register confirmation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  confirmRegister = { 
+    firstName: '', 
+    lastName: '',
+    birth: '',
+    genre: '',
+    email: '',
+    password: '',
+    confirmationPassword: '',
+  }
+
+  toggle = {
+    errorMessage: false,
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  home() {
+    this.router.navigate(['/home/'])
+  }
+
+  register() {
+    if (
+      this.confirmRegister.firstName.length > 0 &&
+      this.confirmRegister.lastName.length > 0 &&
+      this.confirmRegister.birth.length > 0 &&
+      this.confirmRegister.genre.length > 0 &&
+      this.confirmRegister.email.length > 0 &&
+      this.confirmRegister.password.length > 0 &&
+      this.confirmRegister.confirmationPassword.length > 0      
+      ) {
+        this.router.navigate(['/events/']);
+      } else {
+        this.toggle.errorMessage = true;
+      }
+  }
+
+  closeErrorMessage() {
+    this.toggle.errorMessage = false;
+  }
 }
