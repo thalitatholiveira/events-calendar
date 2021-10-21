4b67f803a05ade845899b77922c4e3a779ea0718
Thalita Oliveira <thalita.th.oliveira@gmail.com>
2021-10-21 14:49:49 -0300

    Add user greeting to event list

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/app/service/user.service';
 
 @Component({
   selector: 'app-events',
@@ -40,7 +41,9 @@ export class EventsComponent implements OnInit {
     index: undefined,
   }
 
-  constructor(private router: Router) {}
+  userName = this.userService.register.firstName;
+
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
   }
